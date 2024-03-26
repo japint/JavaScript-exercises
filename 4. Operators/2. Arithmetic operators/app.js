75426d9e5ffe9f669acc3b8e8590d4ed7cc360b8
japint <jap1centeno@gmail.com>
2024-03-27 00:02:46 +0100

    Added Arithmetic and Comparison Operators

---
+++ 4. Operators/2. Arithmetic operators/app.js
@@ -0,0 +1,34 @@
+//Arithmetic operations
+var add = 10, sub = 20, mult = 30, div = 40, expo = 5, modulus = 20;
+console.log('Add: ' + add + ' Result: ' + (add + 10));
+console.log(sub - 10);
+console.log(mult * 10);
+console.log(div / 10);
+console.log(expo ** 10);
+console.log(modulus % 9);
+
+//Increment/Decrement operations
+console.log('Increment/Decrement Operations');
+
+var x = 0, y = 5, z = 0;
+console.log('Postfix increment');
+console.log('First output: ' + x++);
+console.log('Second output: ' + x++);
+console.log('Third output: ' + x);
+
+console.log('Postfix decrement');
+console.log('First output: ' + y--);
+console.log('Second output: ' + y--);
+console.log('Third output: ' + y);
+
+x = 0, y = 5, z = 0;
+
+console.log('Postfix increment');
+console.log('First output: ' + ++x);
+console.log('Second output: ' + ++x);
+console.log('Third output: ' + x);
+
+console.log('Postfix decrement');
+console.log('First output: ' + --y);
+console.log('Second output: ' + --y);
+console.log('Third output: ' + y);
